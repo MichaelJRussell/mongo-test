ce6bea2c94e2969b891d8dc4692e51b388df44f8
Michael Russell <MichaelJRussell@users.noreply.github.com>
2022-07-21 09:07:00 -0500

    Adds class to serialize/deserialize dates/times to Mongo

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,8 @@
+package spring
+
+import mongo.test.MappingContextBeanPostProcessor
+
 // Place your Spring DSL code here
 beans = {
+    mappingContextBeanPostProcessor(MappingContextBeanPostProcessor)
 }
